18010f19ba16fc725fdb6d33b309be1c65002c7b
yukiko0902 <yukikotakeuchi0902@gmail.com>
2023-06-07 12:25:37 +0900

    ユーザー管理実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  
 end
