208d0f1f05b21a7a2630319ab02c4c3a5f30f458
yukiko0902 <yukikotakeuchi0902@gmail.com>
2023-06-20 17:09:28 +0900

    投稿詳細機能②／編集削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
-  resources :diaries, only: [:index,:new, :create, :show]
+  resources :diaries, only: [:index,:new, :create, :show, :edit, :destroy]
 end
